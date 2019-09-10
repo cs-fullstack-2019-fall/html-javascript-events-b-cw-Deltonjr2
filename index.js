f6a2991588cb753f03055fd3c749efd891551949
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-09-10 16:58:34 -0500

    list

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1.let textInputElement=document.querySelector("#labellove");
+    textInputElement.onkeydown= function(e){
+        console.log (e.target.value);
+
+};
+// 2.Using the exercise above, replace the user's typing with the string below. Each time they press a key, a letter from the string below should display in the label instead. There's no need to have the text start over from the beginning.
+
+
+
+
+
+let codeCrewString = "Code School is a hands-on class designed to train individuals to be entry-level software developers within a six-month course. The minimum education requirement is at least a high school diploma or GED. Students work in a small classroom setting and use real-word technologies to learn the fundamentals of coding, app development, the leadership, and the life skills needed for a successful career and job market competitiveness.";
+
+
+
